ac846cc8f21474381c613c0fd063d9e1062a7d07
Jeremy Utting <jeremy@zoidberg.shwep.nz>
2016-04-24 17:58:43 +1200

    script to start the party

---
+++ start_party.py
@@ -0,0 +1,28 @@
+#!/home/jeremy/git/Party-Button/party_env/bin/python
+
+import pychromecast
+import pychromecast.controllers.youtube as youtube
+
+video_url = ""
+video_format = ""
+cast_device = "SHIELD"
+
+#print "Listing available Chromecasts:"
+#print pychromecast.get_chromecasts_as_dict().keys()
+
+print "connecting to cast device:"
+cast = pychromecast.get_chromecast(friendly_name=cast_device)
+cast.wait()
+print cast.device
+print cast.status
+
+#Youtube stuff is broken for now, but might work later
+#yt = youtube.YouTubeController()
+#cast.register_handler(yt)
+
+print "Trying to play media:"
+#yt.play_video("https://www.youtube.com/watch?v=y6120QOlsfU")
+mc = cast.media_controller
+mc.play_media(video_url, video_format)
+
+print mc.status
